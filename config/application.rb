cbfb9c6f9b32e7d1e753612a098a0e7d1031da79
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-05-18 14:24:59 +0100

    Updates to config/application

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,35 +11,5 @@ module Calendars
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/app/services)
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # This saves loading all 80-odd locales from the rails-i18n gem.  This doesn't affect the loading
-    # of locales from config/locales
-    config.i18n.available_locales = [:en, :cy]
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    config.assets.prefix = '/calendars'
-    config.assets.precompile += %w(
-      application.css
-      print.css
-    )
-
-    # Disable Rack::Cache.
-    config.action_dispatch.rack_cache = nil
-
-    config.action_dispatch.ignore_accept_header = true
   end
 end
